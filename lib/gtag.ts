e814a27c09ff05b20bff4c06bbb54f51a4cb31d0
ShivamSV1 <131441312+ShivamSV1@users.noreply.github.com>
2025-06-14 17:31:38 +0530

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,5 +1,12 @@
 export const GA_TRACKING_ID = "G-VG8F111T8P";
 
+// Extend the global Window interface
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 // Log page views
 export const pageview = (url: string): void => {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
