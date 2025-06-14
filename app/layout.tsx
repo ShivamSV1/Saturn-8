8d22c0c7c8175f0fb9e3a602789d7d1700ba008b
ShivamSV1 <131441312+ShivamSV1@users.noreply.github.com>
2025-06-14 16:04:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-VG8F111T8P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VG8F111T8P');
+</script></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
