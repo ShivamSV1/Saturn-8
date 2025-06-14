1c0c0d44152386f0365eda6029a6099cafa03af5
ShivamSV1 <131441312+ShivamSV1@users.noreply.github.com>
2025-06-14 17:29:28 +0530

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,8 +1,10 @@
 export const GA_TRACKING_ID = "G-VG8F111T8P";
 
 // Log page views
-export const pageview = (url) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+export const pageview = (url: string): void => {
+  if (typeof window !== "undefined" && typeof window.gtag === "function") {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
